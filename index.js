0ea721e059f4380c2d062de2a12c03e00d3ff7ed
Steve KINI <fkini92@gmail.com>
2025-04-07 17:39:16 -0400

    Using composite action

--- index.js
+++
@@ -1,8 +0,0 @@
-const core = require('@actions/core');
-
-try {
-    const name = core.getInput('author');
-    console.log(`Hello world, ${name}!`);
-} catch (error) {
-    core.setFailed(`Une erreur est survenue : ${error.message}`);
-}
